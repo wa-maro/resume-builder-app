a31404062a84c84858ea04d34471595dc64ffefd
wa-maro <theo.pombe@gmail.com>
2026-08-26 19:50:10 +0300

    feat(api/errors): enforce Joi.ValidationErrorItem type for details in ValidationError class

--- api/src/shared/errors/validation-error.ts
+++ api/src/shared/errors/validation-error.ts
@@ -1,9 +1,10 @@
 import { AppError } from "./app-error.js";
+import Joi from "joi";
 
 export class ValidationError extends AppError {
-  public readonly details?: unknown;
+  public readonly details: Joi.ValidationErrorItem[];
 
-  constructor(message: string, details?: unknown) {
+  constructor(message: string, details: Joi.ValidationErrorItem[]) {
     super(message, 400);
 
     this.details = details;
