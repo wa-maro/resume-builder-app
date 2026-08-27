80a3c4af71730877fefc47bbec80c5442e036259
wa-maro <theo.pombe@gmail.com>
2026-08-27 21:13:12 +0300

    feat(api/security): implement password hashing service methods

---
+++ api/src/security/password/password.service.ts
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+
+/**
+ * Hash the password in plain text
+ * @param password string
+ * @returns romise string
+ */
+export const doHash = async (password: string): Promise<string> => {
+  return await bcrypt.hash(password, 10);
+};
+
+/**
+ * Compare plain password against encripted version
+ * @param password string
+ * @param hashedPassword string
+ * @returns Promise boolean
+ */
+export const compareHash = async (
+  password: string,
+  hashedPassword: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hashedPassword);
+};
