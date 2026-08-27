5c79a4439ce97e387926e140ff6599899fa525d1
wa-maro <theo.pombe@gmail.com>
2026-08-27 18:49:41 +0300

    feat(api/users): implement User model with schema

---
+++ api/src/modules/users/user.model.ts
@@ -0,0 +1,53 @@
+import mongoose, { type HydratedDocument } from "mongoose";
+import { UserRole, UserWithCredential } from "./user.types.js";
+
+export type UserDocument = HydratedDocument<UserWithCredential>;
+
+// define a User schema
+const UserSchema = new mongoose.Schema<UserWithCredential>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    passwordHash: {
+      type: String,
+      required: true,
+      trim: true,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: Object.values(UserRole),
+      default: UserRole.USER,
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  },
+);
+
+// define a User model
+const UserModel = mongoose.model<UserWithCredential>("User", UserSchema);
+
+export default UserModel;
