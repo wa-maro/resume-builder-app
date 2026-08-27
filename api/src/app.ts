175942e8e3da6f82d9fcb5e9185aebecf1c17a90
wa-maro <theo.pombe@gmail.com>
2026-08-27 09:25:39 +0300

    feat(api/security): enhanced security and request handling

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
 import { httpLogger } from "./shared/utils/loggers.util.js";
 import errorHandler from "./http/middlewares/error-handler.middleware.js";
+import { env } from "./config/env.js";
 
 const app = express();
 
@@ -9,6 +13,25 @@ app.use((req, _res, next) => {
 
   next();
 });
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false, // disable CORP for serving static files
+  }),
+);
+
+app.use(
+  cors({
+    origin: env.frontendOrigin,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.get("/api/v1/hello-world", (_req, res) => {
   return res.status(200).json({
