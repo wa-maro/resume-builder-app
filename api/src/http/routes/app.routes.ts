ef9e815ad8756c940e8ef7bbceb515361bc54e4f
wa-maro <theo.pombe@gmail.com>
2026-08-29 21:47:10 +0300

    feat(api/routes): configure authentication routes

--- api/src/http/routes/app.routes.ts
+++ api/src/http/routes/app.routes.ts
@@ -3,8 +3,11 @@ import adminRouter from "./admin.routes.js";
 import authenticate from "../middlewares/authenticate.middleware.js";
 import authorize from "../middlewares/authorize.middleware.js";
 import { UserRole } from "../../modules/users/user.types.js";
+import authRouter from "../../modules/auth/auth.routes.js";
 
 const appRouter = Router();
+
+appRouter.use("/auth", authRouter);
 
 appRouter.use("/admin", authenticate, authorize(UserRole.ADMIN), adminRouter);
 
