7eff41fa114cb62469edb13628f84a2df2aa524d
wa-maro <theo.pombe@gmail.com>
2026-08-30 07:32:18 +0300

    feat(api/users): add editProfileBodySchema for user update profile request body validation

--- api/src/modules/auth/auth.validation.ts
+++ api/src/modules/auth/auth.validation.ts
@@ -55,3 +55,29 @@ export const loginBodySchema = Joi.object({
     "any.required": "Password is required.",
   }),
 });
+
+export const editProfileBodySchema = Joi.object({
+  newUsername: Joi.string().trim().min(3).max(30).messages({
+    "string.base": "newUsername must be a text.",
+    "string.min": "newUsername must be at least 3 characters long.",
+    "string.max": "newUsername must not exceed 30 characters.",
+  }),
+
+  newEmail: Joi.string()
+    .trim()
+    .lowercase()
+    .pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/)
+    .messages({
+      "string.base": "newEmail must be a text.",
+      "any.required": "newEmail is required.",
+    }),
+
+  newPassword: Joi.string().min(6).messages({
+    "string.base": "newPassword must be a text.",
+    "string.min": "Password must be at least 8 characters long.",
+  }),
+})
+  .min(1)
+  .messages({
+    "object.min": "At least one field must be provided for update",
+  });
