d79746614b41a4ccf903a7e2ed085fabbe6f862e
wa-maro <theo.pombe@gmail.com>
2026-08-30 14:03:09 +0300

    feat(api/database): prepare resumes data for seedling

---
+++ api/src/database/seeders/data/resumes.ts
@@ -0,0 +1,47 @@
+export default [
+  {
+    title: "mechanic",
+    summary:
+      "A skilled mechanic with experience in diagnosing, repairing, and maintaining vehicles and machinery. Adept at troubleshooting complex technical issues and ensuring safety standards are met. Dedicated to providing efficient and reliable service to maximize performance and customer satisfaction.",
+  },
+  {
+    title: "house_painter",
+    summary:
+      "Professional house painter with expertise in surface preparation, paint selection, and high-quality finishes. Skilled in both interior and exterior projects, ensuring precision and durability. Committed to delivering aesthetically pleasing results while meeting client timelines.",
+  },
+  {
+    title: "charcoal_seller",
+    summary:
+      "Entrepreneurial charcoal seller with experience in sourcing, packaging, and distributing quality products. Knowledgeable in customer engagement and maintaining strong supplier networks. Focused on delivering reliable service while promoting sustainable business practices.",
+  },
+  {
+    title: "tailor",
+    summary:
+      "Creative tailor with expertise in garment design, alterations, and fabric selection. Skilled in crafting custom-fitted clothing that meets client needs and fashion trends. Known for precision, attention to detail, and delivering high-quality results.",
+  },
+  {
+    title: "sign_painter",
+    summary:
+      "Talented sign painter specializing in creating clear, attractive, and durable signage for businesses and events. Proficient in hand-painting techniques, lettering styles, and design layouts. Dedicated to producing visually appealing work that enhances branding and visibility.",
+  },
+  {
+    title: "shopkeeper",
+    summary:
+      "Experienced shopkeeper with strong skills in sales, inventory management, and customer service. Capable of handling daily operations efficiently to ensure smooth business performance. Dedicated to fostering positive client relationships and driving consistent revenue growth.",
+  },
+  {
+    title: "electrician",
+    summary:
+      "Professional house painter with expertise in surface preparation, paint selection, and high-quality finishes. Skilled in both interior and exterior projects, ensuring precision and durability. Committed to delivering aesthetically pleasing results while meeting client timelines.",
+  },
+  {
+    title: "sign_painter",
+    summary:
+      "Talented sign painter specializing in creating clear, attractive, and durable signage for businesses and events. Proficient in hand-painting techniques, lettering styles, and design layouts. Dedicated to producing visually appealing work that enhances branding and visibility.",
+  },
+  {
+    title: "plumber",
+    summary:
+      "I am a skilled worker who puts in, fixes, and cares for water pipes, drains, and bathroom or kitchen fixtures in homes and buildings.",
+  },
+];
