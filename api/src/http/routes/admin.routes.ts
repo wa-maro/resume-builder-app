fb96290ae140f8f1f730d82e6c0a6bb4eef827a7
wa-maro <theo.pombe@gmail.com>
2026-08-30 08:40:45 +0300

    feat(api/routes): add resumesAdminRouter for resume management in admin routes

--- api/src/http/routes/admin.routes.ts
+++ api/src/http/routes/admin.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import usersAdminRouter from "../../modules/users/user-admin.routes.js";
+import resumesAdminRouter from "../../modules/resume/routes/resume-admin.routes.js";
 
 const adminRouter = Router();
 
 adminRouter.use("/users", usersAdminRouter);
+adminRouter.use("/resumes", resumesAdminRouter);
 
 export default adminRouter;
---
+++ api/src/modules/resume/routes/resume-admin.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const resumesAdminRouter = Router();
+
+export default resumesAdminRouter;
