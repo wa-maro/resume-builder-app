90e3a577eca701f819885e3e43f77013b9ca880b
wa-maro <theo.pombe@gmail.com>
2026-08-30 09:12:21 +0300

    feat(api/auth): add user login endpoint

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { registerUser } from "./auth.service.js";
+import { loginUser, registerUser } from "./auth.service.js";
 
 export async function register(req: Request, res: Response) {
   const { username, email, password } = req.body;
@@ -8,5 +8,15 @@ export async function register(req: Request, res: Response) {
     success: true,
     message: "User registered successfully",
     data: await registerUser(username, email, password),
+  });
+}
+
+export async function login(req: Request, res: Response) {
+  const { usernameOrEmail, password } = req.body;
+
+  return res.status(200).json({
+    success: true,
+    message: "User logged in successfully",
+    data: await loginUser(usernameOrEmail, password),
   });
 }
--- api/src/modules/auth/auth.routes.ts
+++ api/src/modules/auth/auth.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from "express";
 import validate from "../../http/middlewares/validation.middleware.js";
 import tryCatch from "../../shared/utils/try-catch.util.js";
-import { registerBodySchema } from "./auth.validation.js";
-import { register } from "./auth.controller.js";
+import { loginBodySchema, registerBodySchema } from "./auth.validation.js";
+import { login, register } from "./auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post(
-  "/register",
-  validate({ body: registerBodySchema }),
-  tryCatch(register, "register"),
-);
+authRouter
+  .post(
+    "/register",
+    validate({ body: registerBodySchema }),
+    tryCatch(register, "register"),
+  )
+  .post(
+    "/login",
+    validate({ body: loginBodySchema }),
+    tryCatch(login, "login"),
+  );
 
 export default authRouter;
