689096b40bf8d54adf293c17a8c7d8bc212d3b98
wa-maro <theo.pombe@gmail.com>
2026-08-30 13:59:10 +0300

    feat(api/database): add more user to seeder

--- api/src/database/seeders/data/users.ts
+++ api/src/database/seeders/data/users.ts
@@ -14,6 +14,18 @@ export default [
     role: UserRole.USER,
   },
   {
+    username: "jasper",
+    email: "jasper@example.com",
+    password: "User@123",
+    role: UserRole.USER,
+  },
+  {
+    username: "samsung",
+    email: "samsung@example.com",
+    password: "User@123",
+    role: UserRole.USER,
+  },
+  {
     username: "memosa",
     email: "memosa@example.com",
     password: "User@123",
@@ -22,6 +34,30 @@ export default [
   {
     username: "kidumu",
     email: "kidumu@example.com",
+    password: "User@123",
+    role: UserRole.USER,
+  },
+  {
+    username: "neema-j",
+    email: "neema-j@example.com",
+    password: "User@123",
+    role: UserRole.USER,
+  },
+  {
+    username: "chapio",
+    email: "chapio@example.com",
+    password: "User@123",
+    role: UserRole.USER,
+  },
+  {
+    username: "admin",
+    email: "admin@example.com",
+    password: "Expo@123",
+    role: UserRole.ADMIN,
+  },
+  {
+    username: "lolomike",
+    email: "lolomike@example.com",
     password: "User@123",
     role: UserRole.USER,
   },
