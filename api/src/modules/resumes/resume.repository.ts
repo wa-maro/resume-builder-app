789af92632e55d2cc9388650a4ee54a1e75f4182
wa-maro <theo.pombe@gmail.com>
2026-08-30 17:59:31 +0300

    feat(api/resumes): implement resume repository with findAll and getCount functions

---
+++ api/src/modules/resumes/resume.repository.ts
@@ -0,0 +1,43 @@
+import ResumeModel from "./resume.model.js";
+import { ResumeFilter, ResumeRepoQueryOptions } from "./resume.types.js";
+
+export async function findAll(query: ResumeRepoQueryOptions) {
+  const {
+    filter = {},
+    skip = 0,
+    limit = 10,
+    sort = "createdAt",
+    order = -1,
+  } = query;
+
+  const mongoFilter = buildResumeMongoFilter(filter);
+
+  return ResumeModel.find(mongoFilter)
+    .sort({
+      [sort]: order,
+      _id: -1,
+    })
+    .skip(skip)
+    .limit(limit);
+}
+
+export async function getCount(filter: ResumeFilter) {
+  const mongoFilter = buildResumeMongoFilter(filter);
+  return ResumeModel.countDocuments(mongoFilter).exec();
+}
+
+function buildResumeMongoFilter(filter: ResumeFilter) {
+  const { search, ...rest } = filter;
+
+  if (!search) {
+    return rest;
+  }
+
+  return {
+    ...rest,
+    $or: [
+      { title: { $regex: search, $options: "i" } },
+      { summary: { $regex: search, $options: "i" } },
+    ],
+  };
+}
