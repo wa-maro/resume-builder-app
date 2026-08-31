b92dec08b7341b0e2e5ceb9f4283dbc1bb309793
wa-maro <theo.pombe@gmail.com>
2026-08-31 12:13:40 +0300

    feat(api/resumes): implement delete functionality for resumes by ID

--- api/src/modules/resumes/controllers/resume.controller.ts
+++ api/src/modules/resumes/controllers/resume.controller.ts
@@ -1,5 +1,10 @@
 import type { Request, Response } from "express";
-import { createResume, findResumeByUserId } from "../resume.service.js";
+import {
+  createResume,
+  findResumeByUserId,
+  removeResumeForUser,
+} from "../resume.service.js";
+import { BadRequestError } from "../../../shared/errors/http-errors.js";
 
 export async function createMyResume(req: Request, res: Response) {
   const { id } = req.user;
@@ -19,5 +24,22 @@ export async function getMyResume(req: Request, res: Response) {
     success: true,
     message: "Resume retrieved successfully",
     data: await findResumeByUserId(id),
+  });
+}
+
+export async function deleteMyResume(req: Request, res: Response) {
+  const { id: userId } = req.user;
+  const { resumeId } = req.params;
+
+  if (typeof resumeId !== "string") {
+    throw new BadRequestError();
+  }
+
+  await removeResumeForUser(userId, resumeId);
+
+  return res.status(200).json({
+    success: true,
+    message: "Resume deleted successfully",
+    data: null,
   });
 }
--- api/src/modules/resumes/routes/resume.routes.ts
+++ api/src/modules/resumes/routes/resume.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import tryCatch from "../../../shared/utils/try-catch.util.js";
 import {
   createMyResume,
+  deleteMyResume,
   getMyResume,
 } from "../controllers/resume.controller.js";
 import validate from "../../../http/middlewares/validation.middleware.js";
 import { createResumeBodySchema } from "../resume.validation.js";
+import { paramsWithIDsSchema } from "../../../shared/validators/params-with-id.js";
 
 const resumeRouter = Router();
 
@@ -15,6 +17,11 @@ resumeRouter
     validate({ body: createResumeBodySchema }),
     tryCatch(createMyResume, "createMyResume"),
   )
-  .get("/", tryCatch(getMyResume, "getMyResume"));
+  .get("/", tryCatch(getMyResume, "getMyResume"))
+  .delete(
+    "/:resumeId",
+    validate({ params: paramsWithIDsSchema }),
+    tryCatch(deleteMyResume, "deleteMyResume"),
+  );
 
 export default resumeRouter;
