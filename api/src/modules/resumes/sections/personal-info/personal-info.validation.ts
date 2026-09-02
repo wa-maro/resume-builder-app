9175b9ba010d29df6977dda7b0c31184c1403b1b
wa-maro <theo.pombe@gmail.com>
2026-09-02 17:48:32 +0300

    feat(api/personal-info): add validation schema for adding personal information

---
+++ api/src/modules/resumes/sections/personal-info/personal-info.validation.ts
@@ -0,0 +1,80 @@
+import Joi from "joi";
+import { Disability, Gender, MaritalStatus } from "./personal-info.types.js";
+
+export const addPersonalInfoBodySchema = Joi.object({
+  fullName: Joi.string().required().trim().messages({
+    "string.base": "Full name must be a text.",
+    "string.empty": "Full name is required.",
+  }),
+
+  gender: Joi.string()
+    .valid(...Object.values(Gender))
+    .required()
+    .messages({
+      "any.only": "Gender must be either 'male' or 'female'.",
+      "string.base": "Gender must be a text.",
+      "string.empty": "Gender is required.",
+    }),
+
+  dateOfBirth: Joi.date().required().messages({
+    "date.base": "Date must be a valid date.",
+  }),
+
+  nationality: Joi.string().optional().trim().messages({
+    "string.base": "Nationality must be a text.",
+  }),
+
+  placeOfDomicile: Joi.string().optional().trim().messages({
+    "string.base": "Place of Domicile must be a text.",
+  }),
+
+  maritalStatus: Joi.string()
+    .valid(...Object.values(MaritalStatus))
+    .optional()
+    .trim()
+    .messages({
+      "string.base": "Marital status must be a text.",
+      "any.only":
+        "Marital status must be one of: single, married, divorced, widowed.",
+    }),
+
+  disabilities: Joi.array()
+    .items(Joi.string().valid(...Object.values(Disability)))
+    .unique()
+    .default([Disability.NONE])
+    .custom((value: Disability[], helpers) => {
+      if (value.includes(Disability.NONE) && value.length > 1) {
+        return helpers.error("any.invalid");
+      }
+
+      return value;
+    })
+    .messages({
+      "any.invalid":
+        "If 'none' is selected, no other disabilities can be selected.",
+      "array.base": "Disabilities must be a list of strings.",
+      "array.unique": "Disabilities must not contain duplicates.",
+    }),
+
+  email: Joi.string().trim().lowercase().email().required().messages({
+    "string.base": "Email must be a text.",
+    "string.empty": "Email is required.",
+    "string.email": "Please provide a valid email address.",
+  }),
+
+  phone: Joi.string()
+    .required()
+    .trim()
+    .pattern(/^\+?[0-9]{7,15}$/)
+    .messages({
+      "string.base": "Phone must be a text.",
+      "string.empty": "Phone is required.",
+      "string.pattern.base":
+        "Phone number must be valid (7-15 digits, optional + at start).",
+    }),
+
+  physicalAddress: Joi.string().required().trim().messages({
+    "string.base": "Address must be a text.",
+    "string.empty": "Address is required.",
+  }),
+});
