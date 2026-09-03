678dee148f0421c379529b6415a63eb003838dec
wa-maro <theo.pombe@gmail.com>
2026-09-03 09:58:12 +0300

    feat(api/personal-info): add delete personal info endpoint for admin

--- api/src/modules/resumes/sections/personal-info/controllers/personal-info-admin.controller.ts
+++ api/src/modules/resumes/sections/personal-info/controllers/personal-info-admin.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { BadRequestError } from "../../../../../shared/errors/http-errors.js";
-import { getPersonalInfoForAdmin } from "../personal-info.service.js";
+import {
+  getPersonalInfoForAdmin,
+  removePersonalInfo,
+} from "../personal-info.service.js";
 
 export async function getPersonalInfo(req: Request, res: Response) {
   const { id } = req.params;
@@ -13,5 +16,21 @@ export async function getPersonalInfo(req: Request, res: Response) {
     success: true,
     message: "Personal information retrieved successfully",
     data: await getPersonalInfoForAdmin(id),
+  });
+}
+
+export async function deletePersonalInfo(req: Request, res: Response) {
+  const { id } = req.params;
+
+  if (typeof id !== "string") {
+    throw new BadRequestError();
+  }
+
+  await removePersonalInfo(id);
+
+  return res.status(200).json({
+    success: true,
+    message: "Personal information delete successfully",
+    data: null,
   });
 }
--- api/src/modules/resumes/sections/personal-info/routes/personal-info-admin.routes.ts
+++ api/src/modules/resumes/sections/personal-info/routes/personal-info-admin.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from "express";
 import tryCatch from "../../../../../shared/utils/try-catch.util.js";
-import { getPersonalInfo } from "../controllers/personal-info-admin.controller.js";
+import {
+  deletePersonalInfo,
+  getPersonalInfo,
+} from "../controllers/personal-info-admin.controller.js";
 import { paramsWithIDsSchema } from "../../../../../shared/validators/params-with-id.js";
 import validate from "../../../../../http/middlewares/validation.middleware.js";
 
 const personalInfoAdminRouter = Router();
 
-personalInfoAdminRouter.get(
-  "/:id",
-  validate({ params: paramsWithIDsSchema }),
-  tryCatch(getPersonalInfo, "getPersonalInfo"),
-);
-
+personalInfoAdminRouter
+  .get(
+    "/:id",
+    validate({ params: paramsWithIDsSchema }),
+    tryCatch(getPersonalInfo, "getPersonalInfo"),
+  )
+  .delete(
+    "/:id",
+    validate({ params: paramsWithIDsSchema }),
+    tryCatch(deletePersonalInfo, "deletePersonalInfo"),
+  );
 export default personalInfoAdminRouter;
