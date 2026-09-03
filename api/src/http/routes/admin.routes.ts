6ba89cb0402d978aa5fc11bf8bde28be21bbca6c
wa-maro <theo.pombe@gmail.com>
2026-09-03 09:44:02 +0300

    feat(api/personal-info): add personal info admin routes

--- api/src/http/routes/admin.routes.ts
+++ api/src/http/routes/admin.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import usersAdminRouter from "../../modules/users/user-admin.routes.js";
 import resumesAdminRouter from "../../modules/resumes/routes/resume-admin.routes.js";
+import personalInfoAdminRouter from "../../modules/resumes/sections/personal-info/routes/personal-info-admin.routes.js";
 
 const adminRouter = Router();
 
 adminRouter.use("/users", usersAdminRouter);
 adminRouter.use("/resumes", resumesAdminRouter);
+adminRouter.use("/personal-informations", personalInfoAdminRouter);
 
 export default adminRouter;
---
+++ api/src/modules/resumes/sections/personal-info/routes/personal-info-admin.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const personalInfoAdminRouter = Router();
+
+export default personalInfoAdminRouter;
