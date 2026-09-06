0b5e18eeffb497cc59e5b28d3b0edb0e8f11a5a4
wa-maro <theo.pombe@gmail.com>
2026-09-06 10:32:44 +0300

    feat(api/messages): add fetch messages admin endpoint

--- api/src/modules/messages/controllers/message-admin.controller.ts
+++ api/src/modules/messages/controllers/message-admin.controller.ts
@@ -1,11 +1,46 @@
 import type { Request, Response } from "express";
 import {
   deactivateMessageById,
-  getMessageById,
+  findMessageById,
+  findMessages,
   replyMessageForAdmin,
 } from "@messages/services";
 import { BadRequestError } from "@shared/errors";
-import { ReplyMessageInput } from "@messages/types";
+import {
+  MessageQueryDto,
+  MessageSortField,
+  ReplyMessageInput,
+} from "@messages/types";
+import { SortOrderDto } from "@shared/types";
+
+export async function getMessages(req: Request, res: Response) {
+  const { page, limit, sort, sortOrder, search, isActive, isReplied } =
+    req.query;
+
+  const query: MessageQueryDto = {
+    filter: {},
+  };
+
+  if (page) query.page = Number(page);
+
+  if (limit) query.limit = Number(limit);
+
+  if (sort) query.sort = sort as MessageSortField;
+
+  if (sortOrder) query.sortOrder = sortOrder as SortOrderDto;
+
+  if (search) query.filter!.search = search as string;
+
+  if (isActive) query.filter!.isActive = isActive === "true";
+
+  if (isReplied) query.filter!.isReplied = isReplied === "true";
+
+  return res.status(200).json({
+    success: true,
+    message: "Messages retrieved successfully",
+    ...(await findMessages(query)),
+  });
+}
 
 export async function getMessage(req: Request, res: Response) {
   const { id } = req.params;
@@ -17,7 +52,7 @@ export async function getMessage(req: Request, res: Response) {
   return res.status(200).json({
     success: true,
     message: "Message retrieved successfully",
-    data: await getMessageById(id),
+    data: await findMessageById(id),
   });
 }
 
--- api/src/modules/messages/routes/message-admin.routes.ts
+++ api/src/modules/messages/routes/message-admin.routes.ts
@@ -2,16 +2,25 @@ import { validate } from "@http/middlewares";
 import {
   deactivateMessage,
   getMessage,
+  getMessages,
   replyMessage,
 } from "@messages/controllers";
 import { tryCatch } from "@shared/utils";
 import { paramsWithIDsSchema } from "@shared/validators";
 import { Router } from "express";
-import { replyMessageSchema } from "../message.validation.js";
+import {
+  messagesQuerySchema,
+  replyMessageSchema,
+} from "../message.validation.js";
 
 const messageAdminRouter = Router();
 
 messageAdminRouter
+  .get(
+    "/",
+    validate({ query: messagesQuerySchema }),
+    tryCatch(getMessages, "getMessages"),
+  )
   .get(
     "/:id",
     validate({
