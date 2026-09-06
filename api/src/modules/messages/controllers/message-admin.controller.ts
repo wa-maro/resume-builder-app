8fa2ed9295bd142bd39ee315ae64aebc74a08418
wa-maro <theo.pombe@gmail.com>
2026-09-06 08:54:14 +0300

    feat(api/messages): add endpoint for message deactivation

--- api/src/modules/messages/controllers/message-admin.controller.ts
+++ api/src/modules/messages/controllers/message-admin.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getMessageById } from "@messages/services";
+import { deactivateMessageById, getMessageById } from "@messages/services";
 import { BadRequestError } from "@shared/errors";
 
 export async function getMessage(req: Request, res: Response) {
@@ -13,5 +13,19 @@ export async function getMessage(req: Request, res: Response) {
     success: true,
     message: "Message retrieved successfully",
     data: await getMessageById(id),
+  });
+}
+
+export async function deactivateMessage(req: Request, res: Response) {
+  const { id } = req.params;
+
+  if (typeof id !== "string") {
+    throw new BadRequestError();
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "Message deactivated successfully",
+    data: await deactivateMessageById(id),
   });
 }
--- api/src/modules/messages/routes/message-admin.routes.ts
+++ api/src/modules/messages/routes/message-admin.routes.ts
@@ -1,17 +1,25 @@
 import { validate } from "@http/middlewares";
-import { getMessage } from "@messages/controllers";
+import { deactivateMessage, getMessage } from "@messages/controllers";
 import { tryCatch } from "@shared/utils";
 import { paramsWithIDsSchema } from "@shared/validators";
 import { Router } from "express";
 
 const messageAdminRouter = Router();
 
-messageAdminRouter.get(
-  "/:id",
-  validate({
-    params: paramsWithIDsSchema,
-  }),
-  tryCatch(getMessage, "getMessage"),
-);
+messageAdminRouter
+  .get(
+    "/:id",
+    validate({
+      params: paramsWithIDsSchema,
+    }),
+    tryCatch(getMessage, "getMessage"),
+  )
+  .delete(
+    "/:id",
+    validate({
+      params: paramsWithIDsSchema,
+    }),
+    tryCatch(deactivateMessage, "deactivateMessage"),
+  );
 
 export { messageAdminRouter };
