aac37a01f8d553b5a724262969a249632108d138
wa-maro <theo.pombe@gmail.com>
2026-09-06 19:21:11 +0300

    feat(api/faqs): add fetch faqs admin endpoint

--- api/src/modules/faqs/controllers/faq-admin.controller.ts
+++ api/src/modules/faqs/controllers/faq-admin.controller.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express";
-import { CreateFAQInput } from "@faqs/types";
+import { CreateFAQInput, FAQQueryDto, FAQSortField } from "@faqs/types";
 import { createForAdmin } from "../faq.repository.js";
+import { SortOrderDto } from "@shared/types";
+import { findFAQs } from "@faqs/services";
 
 export async function createFAQAdmin(req: Request, res: Response) {
   const data: CreateFAQInput = req.body;
@@ -9,5 +11,31 @@ export async function createFAQAdmin(req: Request, res: Response) {
     success: true,
     message: "Personal information added successfully",
     data: await createForAdmin(data),
+  });
+}
+
+export async function getFAQsAdmin(req: Request, res: Response) {
+  const { page, limit, sort, sortOrder, search, isActive } = req.query;
+
+  const query: FAQQueryDto = {
+    filter: {},
+  };
+
+  if (page) query.page = Number(page);
+
+  if (limit) query.limit = Number(limit);
+
+  if (sort) query.sort = sort as FAQSortField;
+
+  if (sortOrder) query.sortOrder = sortOrder as SortOrderDto;
+
+  if (search) query.filter!.search = search as string;
+
+  if (isActive) query.filter!.isActive = isActive === "true";
+
+  return res.status(200).json({
+    success: true,
+    message: "FAQs retrieved successfully",
+    ...(await findFAQs(query)),
   });
 }
--- api/src/modules/faqs/routes/faq-admin.routes.ts
+++ api/src/modules/faqs/routes/faq-admin.routes.ts
@@ -1,15 +1,21 @@
 import { tryCatch } from "@shared/utils";
 import { Router } from "express";
 import { validate } from "@http/middlewares";
-import { addFAQSchema } from "../faq.validation.js";
-import { createFAQAdmin } from "@faqs/controllers";
+import { addFAQSchema, faqsQuerySchema } from "../faq.validation.js";
+import { createFAQAdmin, getFAQsAdmin } from "@faqs/controllers";
 
 const faqsAdminRouter = Router();
 
-faqsAdminRouter.post(
-  "/",
-  validate({ body: addFAQSchema }),
-  tryCatch(createFAQAdmin, "createFAQAdmin"),
-);
+faqsAdminRouter
+  .post(
+    "/",
+    validate({ body: addFAQSchema }),
+    tryCatch(createFAQAdmin, "createFAQAdmin"),
+  )
+  .get(
+    "/",
+    validate({ query: faqsQuerySchema }),
+    tryCatch(getFAQsAdmin, "getFAQsAdmin"),
+  );
 
 export { faqsAdminRouter };
