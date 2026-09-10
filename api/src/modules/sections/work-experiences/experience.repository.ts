2711b9129f4239e7982305f30bb3f8a20bb73af0
wa-maro <theo.pombe@gmail.com>
2026-09-10 08:16:12 +0300

    feat(api/work-experiences): implement findAll and getCount repository functions

--- api/src/modules/sections/work-experiences/experience.repository.ts
+++ api/src/modules/sections/work-experiences/experience.repository.ts
@@ -1,1 +1,57 @@
-export const experiencesRepository = {};
+import {
+  WorkExperienceFilter,
+  WorkExperienceRepoQueryOptions,
+} from "@work-experiences/types";
+import { WorkExperienceModel } from "@work-experiences";
+import { PopulateResume } from "@resumes/types";
+
+async function findAll(query: WorkExperienceRepoQueryOptions) {
+  const {
+    filter = {},
+    skip = 0,
+    limit = 10,
+    sort = "createdAt",
+    order = -1,
+  } = query;
+
+  const mongoFilter = buildWorkExperienceMongoFilter(filter);
+
+  return WorkExperienceModel.find(mongoFilter)
+    .populate<PopulateResume>("resume", "_id title")
+    .sort({
+      [sort]: order,
+      _id: -1,
+    })
+    .skip(skip)
+    .limit(limit)
+    .exec();
+}
+
+async function getCount(filter: WorkExperienceFilter) {
+  const mongoFilter = buildWorkExperienceMongoFilter(filter);
+
+  return WorkExperienceModel.countDocuments(mongoFilter).exec();
+}
+
+function buildWorkExperienceMongoFilter(filter: WorkExperienceFilter) {
+  const { search, ...rest } = filter;
+
+  if (!search) {
+    return rest;
+  }
+
+  return {
+    ...rest,
+    $or: [
+      { position: { $regex: search, $options: "i" } },
+      { responsibilities: { $regex: search, $options: "i" } },
+      { "company.name": { $regex: search, $options: "i" } },
+      { "company.location": { $regex: search, $options: "i" } },
+    ],
+  };
+}
+
+export const workExperiencesRepository = {
+  findAll,
+  getCount,
+};
