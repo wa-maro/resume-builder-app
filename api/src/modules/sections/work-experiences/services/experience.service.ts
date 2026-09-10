83b3dce87f2bc77ed3aa1059033e743acb213bed
wa-maro <theo.pombe@gmail.com>
2026-09-10 10:00:38 +0300

    feat(api/work-experiences): implement fetch work experiences functionality by resume id

--- api/src/modules/sections/work-experiences/services/experience.service.ts
+++ api/src/modules/sections/work-experiences/services/experience.service.ts
@@ -1,1 +1,17 @@
-export const experiencesService = {};
+import { resumeService } from "@resumes/services";
+import { workExperiencesRepository } from "../experience.repository.js";
+import { WorkExperienceResponseDto } from "@work-experiences/types";
+
+async function findAllByResume(resumeId: string) {
+  const resume = await resumeService.findResumeById(resumeId);
+
+  const experiences = await workExperiencesRepository.findAllByResume(
+    resume.id,
+  );
+
+  return experiences.map((exp) => new WorkExperienceResponseDto(exp));
+}
+
+export const workExperiencesService = {
+  findAllByResume,
+};
