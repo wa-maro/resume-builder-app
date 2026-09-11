9273d383b5d7b595e9e50fb124cc326bf5cc6331
wa-maro <theo.pombe@gmail.com>
2026-09-11 06:05:59 +0300

    fix(api/faqs): use proper http 200 status code instead of 201

--- api/src/modules/faqs/controllers/faq.controller.ts
+++ api/src/modules/faqs/controllers/faq.controller.ts
@@ -2,7 +2,7 @@ import { faqService } from "@faqs/services";
 import type { Request, Response } from "express";
 
 async function getFaqs(_req: Request, res: Response) {
-  return res.status(201).json({
+  return res.status(200).json({
     success: true,
     message: "FAQs retrieved successfully",
     data: await faqService.findActiveFAQs(),
